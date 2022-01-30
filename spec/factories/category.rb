e8e071c231b8605896263b6eb5543f6d083f8413
andbri321 <andbri321@gmai.com>
2022-01-30 20:09:55 -0300

    [u] factories/category.rb, executar rubocop

--- spec/factories/category.rb
+++ spec/factories/category.rb
@@ -1,14 +1,15 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :category do
-
-    name {"category_#{rand(999999)}@user.com"}
+    name { "category_#{rand(999_999)}@user.com" }
 
     trait :alcoholic_drink do
-      name {"alcoholic_drink"}
+      name { 'alcoholic_drink' }
     end
 
     trait :non_alcoholic_drink do
-      name {"non_alcoholic_drink"}
+      name { 'non_alcoholic_drink' }
     end
 
     factory :category_alcoholic_drink, traits: [:alcoholic_drink]

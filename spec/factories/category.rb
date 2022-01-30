9d6fe7e98a934dca2cfc1b86d4c2f187f8455091
andbri321 <andbri321@gmai.com>
2022-01-30 15:55:43 -0300

    [c] factorie category

---
+++ spec/factories/category.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+  factory :category do
+
+    name {"category_#{rand(999999)}@user.com"}
+
+    trait :alcoholic_drink do
+      name {"alcoholic_drink"}
+    end
+
+    trait :non_alcoholic_drink do
+      name {"non_alcoholic_drink"}
+    end
+
+    factory :category_alcoholic_drink, traits: [:alcoholic_drink]
+    factory :category_non_alcoholic_drink, traits: [:non_alcoholic_drink]
+  end
+end

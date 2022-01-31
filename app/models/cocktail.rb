18ba3f79f7933c6d6f52c113b84b2e3d19a4cb88
andbri321 <andbri321@gmai.com>
2022-01-30 21:20:36 -0300

    [u] models/cocktail.rb, adicionar scope by_name

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
 class Cocktail < ApplicationRecord
+
   belongs_to :category
   has_many :ingredients
+
+  validates :name, presence: true, uniqueness: true
+
+  scope :by_name, lambda { |name|
+    words = name.split(' ')
+    words = words.map.with_index { |w, i| i.zero? ? "#{w}%" : "% #{w}%" }
+    where.like_and(name: words)
+  }
 end

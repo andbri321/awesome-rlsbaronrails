d28d386931ef73279692473f166fe7a6600dd148
andbri321 <andbri321@gmai.com>
2022-01-30 21:05:05 -0300

    [u] routes.rb, adicionar endpoint random

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   namespace :v1 do
-    resources :cocktails
+    resources :cocktails do
+      get 'random', on: :collection
+    end
   end
 end

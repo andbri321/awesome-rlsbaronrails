64d6237f17c379005eccdf43aaba59182d738fb3
andbri321 <andbri321@gmai.com>
2022-02-01 22:01:49 -0300

    [u] /models/concerns/filterable.rb, adicionar metodo filter_by_ingredient

--- app/models/concerns/filterable.rb
+++ app/models/concerns/filterable.rb
@@ -4,9 +4,11 @@ module Filterable
   extend ActiveSupport::Concern
 
   module ClassMethods
-    def filter(params)
-      scope = all
-      filter_by_name(scope, params)
+    def filter(params, scope = all)
+      return scope unless params
+
+      scope = filter_by_name(scope, params)
+      filter_by_ingredient(scope, params)
     end
 
     private
@@ -15,6 +17,12 @@ module Filterable
       return scope unless params[:name]
 
       scope.by_name(params[:name])
+    end
+
+    def filter_by_ingredient(scope, params)
+      return scope unless params[:ingredient]
+
+      scope.by_ingredient_name(params[:ingredient])
     end
   end
 end
